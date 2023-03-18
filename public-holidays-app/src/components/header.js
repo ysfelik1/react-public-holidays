6f71fed521dc4862efa9f2ca21b0cb70926cde87
yusufelik <ysfelik1@gmail.com>
2023-03-18 18:16:11 +0100

    added header copmenent

---
+++ public-holidays-app/src/components/header.js
@@ -0,0 +1,10 @@
+const Header = ({className}) => {
+    return (
+      
+         <div  className={className}>
+            <h1>Public Holidays World Wide</h1>
+         </div>       
+      
+    );
+  };
+  export default Header;
