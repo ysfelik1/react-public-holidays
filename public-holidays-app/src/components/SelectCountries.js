234d3d55d28758e0330abf4dedd7eb79ffa306ca
yusufelik <ysfelik1@gmail.com>
2023-03-23 23:25:21 +0100

    remove extra lines

--- public-holidays-app/src/components/SelectCountries.js
+++ public-holidays-app/src/components/SelectCountries.js
@@ -1,21 +1,12 @@
 import useFetch from "../hooks/useFetch";
 import Loader from "./Loader";
 import ErrorPage from "./ErrorPage";
-// import { useState, useEffect } from 'react';
-
 const SelectCountries = ({ handleCountryChange ,selectedValue}) => {
     const { data, isLoading, error } = useFetch(`https://date.nager.at/api/v3/AvailableCountries`, false);
 
-    // const [selectedCountry, setSelectedCountry] = useState();
-
     const handleSelectChange = (event) => {
-        // setSelectedCountry(event.target.value);
         handleCountryChange(event.target.value);
     };
-
-    // useEffect(() => {
-    //     console.log(selectedCountry);
-    // }, [selectedCountry]);
     
     return isLoading ? <Loader /> : error ? <ErrorPage errorText={error.message} /> : (
         <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example" defaultValue={selectedValue} onChange={handleSelectChange}>
